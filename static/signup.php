a39d14df2c09f598310b7fc935629017900e2250
Encerwal <ginoong.pampanga@gmail.com>
2024-08-16 14:06:54 +0800

    Upload about, contact and signup

---
+++ static/signup.php
@@ -0,0 +1,24 @@
+<?php
+// for checking errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Include the header
+include('header.php');
+?>
+
+<!-- Main Content of the Home Page -->
+<main>
+    <section>
+        <div class="container">
+            <h1>This is the SIGNUP PAGE</h1>
+            <p>yes</p>
+
+        </div>
+    </section>
+</main>
+
+<?php
+// Include the footer
+include('footer.php');
+?>
